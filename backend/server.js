68308c1edb67bea1a601787ad697335397edfa09
ArshitaThakur <arshitathakur488@gmail.com>
2025-11-06 01:38:11 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ const generalLimiter = rateLimit({
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://hoppscotch.io',  'https://witwizhub-1.onrender.com' , 'https://witwiz-hub.vercel.app/login'],
+  origin: ['http://localhost:5173', 'https://hoppscotch.io',  'https://witwizhub-1.onrender.com' , 'https://witwiz-hub.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Authorization', 'Content-Type'],
   credentials: true
