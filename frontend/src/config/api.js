dc0e6ed1371fd48b066081e2a7f263e0485c52e6
Arshita Thakur <arshitathakur488@gmail.com>
2026-02-05 01:55:45 +0530

    added config file

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,11 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-console.log("API_BASE_URL:", API_BASE_URL); 
 
-export default API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+if (!API_BASE_URL) {
+  console.error(
+    "VITE_API_URL is NOT defined. Check Vercel Environment Variables."
+  );
+} else {
+  console.log(" API_BASE_URL loaded:", API_BASE_URL);
+}
