d9af06b8283f252345b8140863e2b33e9503c2a8
Arshita Thakur <arshitathakur488@gmail.com>
2026-05-22 13:21:36 +0530

    add changes

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -18,7 +18,7 @@ function AboutSection() {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
-          About My Site
+          About WitWizHub
         </motion.h2>
         <motion.p
           className="text-2xl tagline-cursive mb-6"
