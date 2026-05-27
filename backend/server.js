e7e4d0cfd4114101d323445044f822478623fd4d
Arshita Thakur <arshitathakur488@gmail.com>
2026-05-27 13:19:26 +0530

    jenkins

--- backend/server.js
+++ backend/server.js
@@ -137,7 +137,7 @@ cron.schedule('0 0 * * *', async () => {
   }
 });
 
-//  error handler
+// General error handler
 app.use((err, req, res, next) => {
   logger.error('Server error:', { error: err.message, stack: err.stack });
   res.status(500).json({ success: false, message: 'Internal server error' });
